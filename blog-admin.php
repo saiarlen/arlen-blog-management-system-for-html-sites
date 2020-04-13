52602d8f910b99f7fb024d329016d6d86cc03ccb
saiarlen <sai.905902@gmail.com>
2020-04-13 22:43:55 +0530

    Initial setup

---
+++ blog-admin.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Arlen Blog Management Login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" type="image/png" href="admin/deps/img/fav.png" />
+
+    <link rel="stylesheet" type="text/css" href="admin/deps/css/bootstrap.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="admin/deps/css/login.css">
+
+</head>
+
+<body>
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100 p-l-85 p-r-85 p-t-55 p-b-55">
+                <form class="login100-form validate-form flex-sb flex-w">
+                    <span class="login100-form-title p-b-32">
+                        Account Login
+                    </span>
+                    <span class="txt1 p-b-11">
+                        Username
+                    </span>
+                    <div class="wrap-input100 validate-input m-b-36" data-validate="Username is required">
+                        <input class="input100" type="text" name="username">
+                        <span class="focus-input100"></span>
+                    </div>
+                    <span class="txt1 p-b-11">
+                        Password
+                    </span>
+                    <div class="wrap-input100 validate-input m-b-12" data-validate="Password is required">
+                        <span class="btn-show-pass">
+                            <i class="fa fa-eye"></i>
+                        </span>
+                        <input class="input100" type="password" name="pass">
+                        <span class="focus-input100"></span>
+                    </div>
+                    <div class="flex-sb-m w-full p-b-48">
+
+                        <div>
+                            <a href="#" class="txt3">
+                                Forgot Password?
+                            </a>
+                        </div>
+                    </div>
+                    <div class="container-login100-form-btn">
+                        <button class="login100-form-btn">
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div id="dropDownSelect1"></div>
+
+    <script src="admin/deps/js/jquery-3.2.1.min.js" type="text/javascript"></script>
+    <script src="admin/deps/js/login.js" type="text/javascript"></script>
+
+
+</body>
+
+</html>
