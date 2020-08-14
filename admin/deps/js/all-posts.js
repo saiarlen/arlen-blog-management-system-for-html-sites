2e922c774309dc6e60daeb1ee80a61870bff0cdf
saiarlen <sai.905902@gmail.com>
2020-08-15 00:00:26 +0530

    All Post Page Pagination Done

--- admin/deps/js/all-posts.js
+++ admin/deps/js/all-posts.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
-    //For Searching 
+    
+
+// ============================================================== 
+// Search bar Script
+// ==============================================================  
+
     $('[data-search]').on('keyup', function () {
 
         var searchVal = $(this).val();
@@ -11,12 +16,15 @@ $(document).ready(function () {
             $('[data-filter-item][data-filter-name*="' + searchVal.toLowerCase() + '"]').removeClass('pag-dis');
         } else {
             filterItems.removeClass('searchhidden');
-            $('.page-item.active a').trigger('click');
+            $('.page-item.active a').trigger('click'); //trigger pagination box when search val empty
         }
     });
 
 
-    //For Pagination Script
+
+// ============================================================== 
+// For Pagination Scrip
+// ==============================================================  
 
     pageSize = 1;
 
@@ -25,19 +33,24 @@ $(document).ready(function () {
         
 
         for (var i = 0; i < pageCount; i++) {
-            if (i == 0)
+            if (i == 0){
                 $(".pagination").append('<li class="page-item active"><a class="page-link" href="javascript:void(0)">' + (i + 1) + '</a></li>');
-            else
+                $(".pre").addClass("pag-disable");// For adding Arrow disable on load
+            }    
+            else{
                 $(".pagination").append('<li class="page-item"><a class="page-link" href="javascript:void(0)">' + (i + 1) + '</a></li>');
+            }
+                
         }
 
 
         showPage(1);
 
         $(".pagination li").click(function () {
-            $('[data-search]').val('');
-            $('[data-filter-item]').removeClass('searchhidden');
+            $('[data-search]').val(''); // Search Bar Val empty on click pagination box
+            $('[data-filter-item]').removeClass('searchhidden'); // unhide all post cads key on search box
             $(".pagination li").removeClass("active");
+            $(".pre").removeClass("pag-disable"); // For Removing Arrow disable
             $(this).addClass("active");
             showPage(parseInt($(this).text()))
         });
@@ -84,18 +97,27 @@ $(document).ready(function () {
         return false;
     });
 
-    $(".page-item a").click(function () {
+
+    $(".page-item a").click(function () { //Pagniation previous and next buttons disable Condition
         if ($(".page-item.active").is(':last-child')) {
+            $(".pre").removeClass("pag-disable");
             $(".nex").addClass("pag-disable");
         } 
         else if ($(".page-item.active").is(':first-child')){
+            $(".nex").removeClass("pag-disable");
             $(".pre").addClass("pag-disable");
         }
         else {
+            
+            $(".pre").removeClass("pag-disable");
             $(".nex").removeClass("pag-disable");
         }
     });
-   
+
+
+// ============================================================== 
+// 
+// ==============================================================
 
 
 
