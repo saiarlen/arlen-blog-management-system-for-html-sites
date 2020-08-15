478c049a4175975ffe5e86581e49fb71c95b07b0
saiarlen <sai.905902@gmail.com>
2020-08-15 22:13:49 +0530

    Heading Correction

--- admin/post-add-new.php
+++ admin/post-add-new.php
@@ -17,7 +17,7 @@ require_once("header.php");
 <div class="page-breadcrumb" style="margin-bottom: 15px;">
     <div class="row">
         <div class="col-12 d-flex no-block align-items-center">
-            <h4 class="page-title">Add Post</h4>
+            <h4 class="page-title">Add New Post</h4>
             <div class="ml-auto text-right">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
