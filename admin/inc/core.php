71b79e8ce8bc52011742a6d4175d8f388cdba0f7
saiarlen <sai.905902@gmail.com>
2020-08-16 19:52:25 +0530

    added excerpt limit function

--- admin/inc/core.php
+++ admin/inc/core.php
@@ -10,7 +10,16 @@
 
  require_once ("config.php");
 
- /*  */
+ /* General Fuctions */
+
+ function limit_excerpt($text, $limit) {
+    if (str_word_count($text, 0) > $limit) {
+        $words = str_word_count($text, 2);
+        $pos   = array_keys($words);
+        $text  = substr($text, 0, $pos[$limit]) . '...';
+    }
+    return $text;
+}
    
 
 
