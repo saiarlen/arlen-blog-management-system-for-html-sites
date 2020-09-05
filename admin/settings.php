733c2f7a06b23d5889271c6e1ed36fdfde906424
saiarlen <sai.905902@gmail.com>
2020-09-05 22:07:42 +0530

    Php mailer added

---
+++ admin/settings.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * Settings page
+ * This page used for app settings
+ * @author    Saiarlen
+ * @url       http://saiarlen.com
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html
+ */
+
+$page_title= "Media"; //page-title
+require_once("header.php");
+?>
+<!-- ============================================================== -->
+<!-- Bread crumb and right sidebar toggle -->
+<!-- ============================================================== -->
+<div class="page-breadcrumb">
+    <div class="row">
+        <div class="col-12 d-flex no-block align-items-center">
+            <h4 class="page-title">Settings</h4>
+            <div class="ml-auto text-right">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="home.php">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Settings</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="disqus_thread"></div>
+<script>
+
+/**
+*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+
+var disqus_config = function () {
+this.page.url ="http://localhost/blog/admin/settings.php";  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = "1212"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://saiarlen.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
+<!-- <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript> -->
+                            
+
+
+
+<?php require_once("footer.php"); ?>
