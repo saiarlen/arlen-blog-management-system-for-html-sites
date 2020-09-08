73ccf0957bbb843579f670549808ee1a6580c9cf
saiarlen <sai.905902@gmail.com>
2020-09-08 21:43:17 +0530

    bug fix

--- admin/inc/user-recover.php
+++ admin/inc/user-recover.php
@@ -57,8 +57,8 @@ if(mysqli_num_rows($armailcheck) == 1){
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'sai.digitalozone@gmail.com';                     // SMTP username
-        $mail->Password   = 'Dsp@8096';                               // SMTP password
+        $mail->Username   = 'sai.xxxx@gmail.com';                     // SMTP username
+        $mail->Password   = 'xxxx';                               // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
